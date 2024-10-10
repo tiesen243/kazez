2478bbd1af2412f1732a514a9b6f2d8eccc90bc4
tiesen243 <ttien56906@gmail.com>
2024-10-10 14:43:55 +0700

    feat: add metadata generation for user profile page
    - Added `generateMetadata` function to generate SEO metadata for user profile
      pages.
    - Updated `Page` component to accept `params` prop.
    - Modified `User` model in Prisma schema to make `name` field required.

--- app/u/[id]/page.tsx
+++ app/u/[id]/page.tsx
@@ -1,7 +1,29 @@
-import type { NextPage } from 'next'
+import type { NextPage, ResolvingMetadata } from 'next'
+import { notFound } from 'next/navigation'
 
-const Page: NextPage = () => {
-  return <div>Page</div>
+import { seo } from '@/lib/seo'
+import { db } from '@/server/db'
+
+interface Props {
+  params: { id: string }
+}
+
+const Page: NextPage<Props> = ({ params }) => {
+  return <main className="container py-4">Page</main>
+}
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata) {
+  const user = await db.user.findUnique({ where: { id: params.id } })
+  if (!user) return notFound()
+
+  const previousImages = (await parent).openGraph?.images ?? []
+
+  return seo({
+    title: user.name ?? user.username,
+    description: `Profile of ${user.name ?? user.username}`,
+    images: [user.avatar, ...previousImages],
+    url: `/u/${params.id}`,
+  })
 }
 
 export default Page
