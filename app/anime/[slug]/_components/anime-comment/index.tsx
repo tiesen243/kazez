54bf7ba92f7fb9064ad70365f9afa7e72038c4d7
tiesen243 <ttien56906@gmail.com>
2024-10-10 21:20:32 +0700

    feat: enhance comment section with avatars and timestamps
    - Replaced Card components with Avatar components for user comments
    - Added user avatars and fallback initials for comments
    - Displayed comment creation date in a readable format
    - Updated Prisma schema to include createdAt field for comments

--- app/anime/[slug]/_components/anime-comment/index.tsx
+++ app/anime/[slug]/_components/anime-comment/index.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from '@tanstack/react-query'
 
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Typography } from '@/components/ui/typography'
 import { useSession } from '@/hooks/use-session'
@@ -22,27 +22,54 @@ export const AnimeComment: React.FC<{ id: string }> = ({ id }) => {
       <section className="mt-4 flex flex-col gap-4">
         <Typography level="h2">Comments</Typography>
         {Array.from({ length: 5 }).map((_, index) => (
-          <Skeleton key={index} className="h-16 w-full" />
+          <div key={index} className="pb-4">
+            <div className="flex items-center gap-2">
+              <Skeleton className="z-10 size-8 rounded-full" />
+              <div className="flex flex-col gap-1">
+                <Skeleton className="h-5 w-20" />
+                <Skeleton className="h-3 w-16" />
+              </div>
+            </div>
+
+            <div className="absolute left-4 top-1 h-full w-[1px] animate-pulse bg-secondary" />
+
+            <Skeleton className="ml-10 mr-16 mt-2 h-6 w-full" />
+          </div>
         ))}
       </section>
     )
 
   return (
-    <section className="mt-4 flex flex-col gap-4">
-      <Typography level="h2">Comments</Typography>
+    <section className="mt-4 flex flex-col">
+      <Typography level="h2" className="mb-4">
+        Comments
+      </Typography>
       {!data?.data ? (
         <p>No comments yet</p>
       ) : (
         data.data.map((comment) => (
-          <Card key={comment.id}>
+          <div key={comment.id} className="pb-4">
             {session?.userId === comment.userId && (
               <RemoveComment id={comment.id} refetch={refetch} />
             )}
-            <CardHeader className="p-4">
-              <CardDescription>{comment.user.name}</CardDescription>
-              <CardTitle>{comment.content}</CardTitle>
-            </CardHeader>
-          </Card>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="z-10 size-8">
+                <AvatarImage src={comment.user.avatar} />
+                <AvatarFallback>{comment.user.username[0]}</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col gap-1">
+                <p className="text-sm">{comment.user.name}</p>
+                <span className="text-xs text-muted-foreground">
+                  {comment.createdAt.toDateString()}
+                </span>
+              </div>
+            </div>
+
+            <div className="absolute left-4 top-1 h-full w-[1px] bg-muted-foreground" />
+
+            <p className="break-all pl-10 pr-16 pt-2">{comment.content}</p>
+          </div>
         ))
       )}
 
